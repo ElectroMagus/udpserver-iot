98c400f5a6731bd9f0bd88831c38534503bdc687
Richard Salmon <ricky@salmonfamily.net>
2022-05-26 11:52:28 -0500

    cleaned up old stuff and added comments

--- tools/udpclient.py
+++ tools/udpclient.py
@@ -3,7 +3,7 @@ import socket
 import sys
  
 
-args                = len(sys.argv)
+args = len(sys.argv)
 if args <  3:
     print("Invalid Syntax:   %s <host> <port> <data>"% sys.argv[0])
     sys.exit(1)
@@ -19,15 +19,12 @@ serverAddressPort   = (host, 20001)
 # Create a UDP socket at client side
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-counter = 1
-while counter <= 1:
-
-    # Send to server using created UDP socket
-    #msgFromClient = str(counter)
-    bytesToSend = str.encode(msgFromClient)
-    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-    UDPClientSocket.settimeout(3)
-    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-    msg = msgFromServer[0]
-    print("%s: %s" % (counter, msg))
-    counter += 1
+# Send to server using created UDP socket
+bytesToSend = str.encode(msgFromClient)
+UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+# Specify timeout for failed connections to prevvent long hangs
+UDPClientSocket.settimeout(3)
+# Recieve Reponse from Server
+msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+msg = msgFromServer[0]
+print("Server Response: %s" % (msg))
