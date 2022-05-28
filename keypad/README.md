54cfd4801c771bad3569e030414e293e0b90bf76
Richard Salmon <ricky@salmonfamily.net>
2022-05-28 13:10:11 -0500

    Adding code for ESP8266 UDP Client Keypad Controller

---
+++ keypad/README.md
@@ -0,0 +1,4 @@
+# UDP Keypad Controller
+ESP8266 and an Adafruit Trellis keypad used to control an ESP32 board with servos and motors attached.  Using UDP to control.
+
+This is testing for a larger project and trying to share what I learn along the way.
---
+++ keypad/src/main.cpp
@@ -0,0 +1,92 @@
+#include "Arduino.h"
+// OTA Update Libraries
+#include <ESP8266Wifi.h>
+//#include <ESPmDNS.h>          
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+const char* ssid = "";
+const char* password = "";
+
+// UDP Libraries
+#include "ESPAsyncUDP.h"
+AsyncUDP udp;
+
+#include "Adafruit_NeoTrellis.h"
+#include "SPI.h"
+Adafruit_NeoTrellis trellis;
+
+// Input a value 0 to 255 to get a color value.
+// The colors are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+   return trellis.pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   return trellis.pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return trellis.pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  return 0;
+}
+
+
+
+// Callback function that lights up all keys when pressed.   Added in UDP code to send a packet on each keypress in the format to speak with the ESP module controlling servos.
+TrellisCallback blink(keyEvent evt){
+  if (evt.bit.EDGE == SEESAW_KEYPAD_EDGE_RISING) {                              // Key pressed
+    trellis.pixels.setPixelColor(evt.bit.NUM, Wheel(map(evt.bit.NUM, 0, trellis.pixels.numPixels(), 0, 255)));      //  Set color on press
+    if(udp.connect(IPAddress(10,9,8,142), 20001)) { 
+      udp.printf("F%i", evt.bit.NUM);                                           // evt.bit.NUM is the key # pressed in the event
+    }
+  } else if (evt.bit.EDGE == SEESAW_KEYPAD_EDGE_FALLING) {                    // Key released
+      trellis.pixels.setPixelColor(evt.bit.NUM, 0);                           // Turn color off on release
+  }
+  trellis.pixels.show();                                                      // Send neopixel color update
+  return 0;
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  // OTA Setup
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  ArduinoOTA.setHostname("trellis");                // mDNS hostname
+  ArduinoOTA.begin();
+  
+  // Initialize Keypad
+  if (!trellis.begin()) {
+    Serial.println("Could not start trellis, check wiring?");
+    while(1) delay(1);
+  } else {
+    Serial.println("NeoPixel Trellis started");
+  }
+
+  // Activate all keys and set their callback to the blink function
+  for(int i=0; i<NEO_TRELLIS_NUM_KEYS; i++){
+    trellis.activateKey(i, SEESAW_KEYPAD_EDGE_RISING);
+    trellis.activateKey(i, SEESAW_KEYPAD_EDGE_FALLING);
+    trellis.registerCallback(i, blink);
+  }
+
+  // Turn on pretty animation   ---   Add to function/callback for nice effect.
+  for (uint16_t i=0; i<trellis.pixels.numPixels(); i++) {
+    trellis.pixels.setPixelColor(i, Wheel(map(i, 0, trellis.pixels.numPixels(), 0, 255)));
+    trellis.pixels.show();
+    delay(50);
+  }
+  for (uint16_t i=0; i<trellis.pixels.numPixels(); i++) {
+    trellis.pixels.setPixelColor(i, 0x000000);
+    trellis.pixels.show();
+    delay(50);
+  }
+}
+
+void loop() {
+  trellis.read();                 // interrupt management handles keypresses
+  delay(20);                      //the trellis has a resolution of around 60hz
+
+  ArduinoOTA.handle();
+  
+}
